75497c8cf9f036c42ee13bbcff1557b049434b7d
Agustín Martinez Suñé <4691852+aemartinez@users.noreply.github.com>
2024-08-22 14:25:25 +0100

    tasks 1-based indexed, experiments save json plan

--- planners.py
+++ planners.py
@@ -2,6 +2,7 @@ import backoff
 import json
 import openai
 
+from collections import namedtuple
 from pydantic_generator import PydanticModelGenerator
 from utils import openai_client
 from juliacall import Main as jl
@@ -9,10 +10,11 @@ from juliacall import Main as jl
 # Initialize Julia and load PDDL package
 jl.seval('using PDDL, SymbolicPlanners')
 
+PlannerResult = namedtuple("PlannerResult", ["plan_pddl", "plan_json", "task_pddl"])
+
 class BasePlanner:
-    
-    def run_planner(self, task_nl, domain_nl, domain_pddl):
-        pass
+    def run_planner(self, task_nl, domain_nl, domain_pddl) -> PlannerResult:
+        raise NotImplementedError
 
     def set_context(self, context):
         self.context = context
@@ -59,36 +61,47 @@ class BasePlanner:
 
 class BaseLlmPlanner(BasePlanner):
 
-    def run_planner(self, task_nl, domain_nl, domain_pddl):
+    def run_planner(self, task_nl, domain_nl, domain_pddl) -> PlannerResult:
         
         prompt = self._create_prompt(task_nl, domain_nl)
-        json_structured_plan = self._query_llm(prompt, domain_pddl)
-        pddl_structured_plan = self._translate_json_to_pddl(json_structured_plan)
+        plan_json = self._query_llm(prompt, domain_pddl)
+        plan_pddl = self._translate_json_to_pddl(plan_json)
 
-        return pddl_structured_plan, None
+        res = PlannerResult(
+            plan_pddl=plan_pddl, 
+            plan_json=plan_json,
+            task_pddl=None
+            )
+
+        return res
 
     def _translate_json_to_pddl(self, json_structured_plan: str):
-        pddl_plan = []
+        plan_pddl = []
 
         plan_dict = json.loads(json_structured_plan)
         
         for step in plan_dict["steps"]:
             action_name = step["action_name"]
             arguments = [value for key, value in step.items() if key != "action_name"]
-            pddl_plan.append(f"({action_name} {' '.join(arguments)})")
+            plan_pddl.append(f"({action_name} {' '.join(arguments)})")
         
-        return "\n".join(pddl_plan)
+        return "\n".join(plan_pddl)
 
 class BaseLlmPddlPlanner(BasePlanner):
 
-    def run_planner(self, task_nl, domain_nl, domain_pddl):
+    def run_planner(self, task_nl, domain_nl, domain_pddl) -> PlannerResult:
         
         prompt = self._create_prompt(task_nl, domain_nl)
         task_pddl = self._query_llm(prompt)
+        plan_pddl = self._run_symbolic_planner(domain_pddl, task_pddl)
 
-        plan = self._run_symbolic_planner(domain_pddl, task_pddl)
+        res = PlannerResult(
+            plan_pddl=plan_pddl, 
+            plan_json=None,
+            task_pddl=task_pddl
+            )
 
-        return plan, task_pddl
+        return res
 
     def _run_symbolic_planner(self, domain_pddl_text, problem_pddl_text):
 
@@ -191,119 +204,119 @@ class LlmSbSPlanner(LlmPlanner):
         prompt += " \nPlease think step by step."
         return prompt
 
-class LlmTotPlanner(BasePlanner):
-    """
-    Tree of Thoughts planner
-    """
+# class LlmTotPlanner(BasePlanner):
+#     """
+#     Tree of Thoughts planner
+#     """
 
-    def run_planner(self, task_nl, domain_nl, domain_pddl, time_left=200, max_depth=2):
-        context = self.context
-        from queue import PriorityQueue
-        start_time = time.time()
-        plan_queue = PriorityQueue()
-        plan_queue.put((0, ""))
-        while time.time() - start_time < time_left and not plan_queue.empty():
-            priority, plan = plan_queue.get()
-            # print (priority, plan)
-            steps = plan.split('\n')
-            if len(steps) > max_depth:
-                return "", None
-            candidates_prompt = self._create_llm_tot_ic_prompt(task_nl, domain_nl, context, plan)
-            candidates = self._query_llm(candidates_prompt).strip()
-            print (candidates)
-            lines = candidates.split('\n')
-            for line in lines:
-                if time.time() - start_time > time_left:
-                    break
-                if len(line) > 0 and '->' in line:
-                    new_plan = plan + "\n" + line
-                    value_prompt = self._create_llm_tot_ic_value_prompt(task_nl, domain_nl, context, new_plan)
-                    answer = self._query_llm(value_prompt).strip().lower()
-                    print(new_plan)
-                    print("Response \n" + answer)
+#     def run_planner(self, task_nl, domain_nl, domain_pddl, time_left=200, max_depth=2) -> PlannerResult:
+#         context = self.context
+#         from queue import PriorityQueue
+#         start_time = time.time()
+#         plan_queue = PriorityQueue()
+#         plan_queue.put((0, ""))
+#         while time.time() - start_time < time_left and not plan_queue.empty():
+#             priority, plan = plan_queue.get()
+#             # print (priority, plan)
+#             steps = plan.split('\n')
+#             if len(steps) > max_depth:
+#                 return "", None
+#             candidates_prompt = self._create_llm_tot_ic_prompt(task_nl, domain_nl, context, plan)
+#             candidates = self._query_llm(candidates_prompt).strip()
+#             print (candidates)
+#             lines = candidates.split('\n')
+#             for line in lines:
+#                 if time.time() - start_time > time_left:
+#                     break
+#                 if len(line) > 0 and '->' in line:
+#                     new_plan = plan + "\n" + line
+#                     value_prompt = self._create_llm_tot_ic_value_prompt(task_nl, domain_nl, context, new_plan)
+#                     answer = self._query_llm(value_prompt).strip().lower()
+#                     print(new_plan)
+#                     print("Response \n" + answer)
 
-                    if "reached" in answer:
-                        return new_plan, None
+#                     if "reached" in answer:
+#                         return new_plan, None
 
-                    if "impossible" in answer:
-                        continue
+#                     if "impossible" in answer:
+#                         continue
 
-                    if "answer: " in answer:
-                        answer = answer.split("answer: ")[1]
+#                     if "answer: " in answer:
+#                         answer = answer.split("answer: ")[1]
 
-                    try:
-                        score = float(answer)
-                    except ValueError:
-                        continue
+#                     try:
+#                         score = float(answer)
+#                     except ValueError:
+#                         continue
 
-                    if score > 0:
-                        new_priority = priority + 1 / score
-                        plan_queue.put((new_priority, new_plan))
+#                     if score > 0:
+#                         new_priority = priority + 1 / score
+#                         plan_queue.put((new_priority, new_plan))
 
-        return "", None
+#         return ""
 
-    def _create_llm_tot_ic_prompt(self, task_nl, domain_nl, context, plan):
-        context_nl, context_pddl, context_sol = context
-        prompt = f"Given the current state, provide the set of feasible actions and their corresponding next states, using the format 'action -> state'. \n" + \
-                 f"Keep the list short. Think carefully about the requirements of the actions you select and make sure they are met in the current state. \n" + \
-                 f"Start with actions that are most likely to make progress towards the goal. \n" + \
-                 f"Only output one action per line. Do not return anything else. " + \
-                 f"Here are the rules. \n {domain_nl} \n\n" + \
-                 f"An example planning problem is: \n {context_nl} \n" + \
-                 f"A plan for the example problem is: \n {context_sol} \n" + \
-                 f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
-                 f"You have taken the following actions: \n {plan} \n"
-        # print(prompt)
-        return prompt
+#     def _create_llm_tot_ic_prompt(self, task_nl, domain_nl, context, plan):
+#         context_nl, context_pddl, context_sol = context
+#         prompt = f"Given the current state, provide the set of feasible actions and their corresponding next states, using the format 'action -> state'. \n" + \
+#                  f"Keep the list short. Think carefully about the requirements of the actions you select and make sure they are met in the current state. \n" + \
+#                  f"Start with actions that are most likely to make progress towards the goal. \n" + \
+#                  f"Only output one action per line. Do not return anything else. " + \
+#                  f"Here are the rules. \n {domain_nl} \n\n" + \
+#                  f"An example planning problem is: \n {context_nl} \n" + \
+#                  f"A plan for the example problem is: \n {context_sol} \n" + \
+#                  f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
+#                  f"You have taken the following actions: \n {plan} \n"
+#         # print(prompt)
+#         return prompt
 
-    def _create_llm_tot_ic_value_prompt(self, task_nl, domain_nl, context, plan):
-        context_nl, context_pddl, context_sol = context
-        context_sure_1 = context_sol.split('\n')[0]
-        context_sure_2 = context_sol.split('\n')[0] + context_sol.split('\n')[1]
-        context_impossible_1 = '\n'.join(context_sol.split('\n')[1:])
-        context_impossible_2 = context_sol.split('\n')[-1]
-        '''
-        prompt = f"Evaluate if a given plan reaches the goal or is an optimal partial plan towards the goal (reached/sure/maybe/impossible). \n" + \
-                 f"Only answer 'reached' if the goal conditions are reached by the exact plan in the prompt. \n" + \
-                 f"Only answer 'sure' if you are sure that preconditions are satisfied for all actions in the plan, and the plan makes fast progress towards the goal. \n" + \
-                 f"Answer 'impossible' if one of the actions has unmet preconditions. \n" + \
-                 f"Here are the rules. \n {domain_nl} \n\n" + \
-                 f"Here are some example evaluations for the planning problem: \n {context_nl} \n\n " + \
-                 f"Plan: {context_sure_1} \n" + \
-                 f"Answer: Sure. \n\n" + \
-                 f"Plan: {context_sure_2} \n" + \
-                 f"Answer: Sure. \n\n" + \
-                 f"Plan: {context_sol} \n" + \
-                 f"Answer: Reached. \n\n" + \
-                 f"Plan: {context_impossible_1} \n" + \
-                 f"Answer: Impossible. \n\n" + \
-                 f"Plan: {context_impossible_2} \n" + \
-                 f"Answer: Impossible. \n\n" + \
-                 f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
-                 f"Evaluate the following partial plan as reached/sure/maybe/impossible. DO NOT RETURN ANYTHING ELSE. DO NOT TRY TO COMPLETE THE PLAN. \n" + \
-                 f"Plan: {plan} \n"
-        '''
-        prompt = f"Determine if a given plan reaches the goal or give your confidence score that it is an optimal partial plan towards the goal (reached/impossible/0-1). \n" + \
-                 f"Only answer 'reached' if the goal conditions are reached by the exact plan in the prompt. \n" + \
-                 f"Answer 'impossible' if one of the actions has unmet preconditions. \n" + \
-                 f"Otherwise,give a number between 0 and 1 as your evaluation of the partial plan's progress towards the goal. \n" + \
-                 f"Here are the rules. \n {domain_nl} \n\n" + \
-                 f"Here are some example evaluations for the planning problem: \n {context_nl} \n\n " + \
-                 f"Plan: {context_sure_1} \n" + \
-                 f"Answer: 0.8. \n\n" + \
-                 f"Plan: {context_sure_2} \n" + \
-                 f"Answer: 0.9. \n\n" + \
-                 f"Plan: {context_sol} \n" + \
-                 f"Answer: Reached. \n\n" + \
-                 f"Plan: {context_impossible_1} \n" + \
-                 f"Answer: Impossible. \n\n" + \
-                 f"Plan: {context_impossible_2} \n" + \
-                 f"Answer: Impossible. \n\n" + \
-                 f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
-                 f"Evaluate the following partial plan as reached/impossible/0-1. DO NOT RETURN ANYTHING ELSE. DO NOT TRY TO COMPLETE THE PLAN. \n" + \
-                 f"Plan: {plan} \n"
+#     def _create_llm_tot_ic_value_prompt(self, task_nl, domain_nl, context, plan):
+#         context_nl, context_pddl, context_sol = context
+#         context_sure_1 = context_sol.split('\n')[0]
+#         context_sure_2 = context_sol.split('\n')[0] + context_sol.split('\n')[1]
+#         context_impossible_1 = '\n'.join(context_sol.split('\n')[1:])
+#         context_impossible_2 = context_sol.split('\n')[-1]
+#         '''
+#         prompt = f"Evaluate if a given plan reaches the goal or is an optimal partial plan towards the goal (reached/sure/maybe/impossible). \n" + \
+#                  f"Only answer 'reached' if the goal conditions are reached by the exact plan in the prompt. \n" + \
+#                  f"Only answer 'sure' if you are sure that preconditions are satisfied for all actions in the plan, and the plan makes fast progress towards the goal. \n" + \
+#                  f"Answer 'impossible' if one of the actions has unmet preconditions. \n" + \
+#                  f"Here are the rules. \n {domain_nl} \n\n" + \
+#                  f"Here are some example evaluations for the planning problem: \n {context_nl} \n\n " + \
+#                  f"Plan: {context_sure_1} \n" + \
+#                  f"Answer: Sure. \n\n" + \
+#                  f"Plan: {context_sure_2} \n" + \
+#                  f"Answer: Sure. \n\n" + \
+#                  f"Plan: {context_sol} \n" + \
+#                  f"Answer: Reached. \n\n" + \
+#                  f"Plan: {context_impossible_1} \n" + \
+#                  f"Answer: Impossible. \n\n" + \
+#                  f"Plan: {context_impossible_2} \n" + \
+#                  f"Answer: Impossible. \n\n" + \
+#                  f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
+#                  f"Evaluate the following partial plan as reached/sure/maybe/impossible. DO NOT RETURN ANYTHING ELSE. DO NOT TRY TO COMPLETE THE PLAN. \n" + \
+#                  f"Plan: {plan} \n"
+#         '''
+#         prompt = f"Determine if a given plan reaches the goal or give your confidence score that it is an optimal partial plan towards the goal (reached/impossible/0-1). \n" + \
+#                  f"Only answer 'reached' if the goal conditions are reached by the exact plan in the prompt. \n" + \
+#                  f"Answer 'impossible' if one of the actions has unmet preconditions. \n" + \
+#                  f"Otherwise,give a number between 0 and 1 as your evaluation of the partial plan's progress towards the goal. \n" + \
+#                  f"Here are the rules. \n {domain_nl} \n\n" + \
+#                  f"Here are some example evaluations for the planning problem: \n {context_nl} \n\n " + \
+#                  f"Plan: {context_sure_1} \n" + \
+#                  f"Answer: 0.8. \n\n" + \
+#                  f"Plan: {context_sure_2} \n" + \
+#                  f"Answer: 0.9. \n\n" + \
+#                  f"Plan: {context_sol} \n" + \
+#                  f"Answer: Reached. \n\n" + \
+#                  f"Plan: {context_impossible_1} \n" + \
+#                  f"Answer: Impossible. \n\n" + \
+#                  f"Plan: {context_impossible_2} \n" + \
+#                  f"Answer: Impossible. \n\n" + \
+#                  f"Now I have a new planning problem and its description is: \n {task_nl} \n" + \
+#                  f"Evaluate the following partial plan as reached/impossible/0-1. DO NOT RETURN ANYTHING ELSE. DO NOT TRY TO COMPLETE THE PLAN. \n" + \
+#                  f"Plan: {plan} \n"
 
-        return prompt
+#         return prompt
 
 available_planners = {
     "llm_ic_pddl"   : LlmIcPddlPlanner(),
@@ -311,5 +324,5 @@ available_planners = {
     "llm"           : LlmPlanner(),
     "llm_ic"        : LlmIcPlanner(),
     "llm_stepbystep": LlmSbSPlanner(),
-    "llm_tot_ic"    : LlmTotPlanner() 
+    # "llm_tot_ic"    : LlmTotPlanner() 
 }
